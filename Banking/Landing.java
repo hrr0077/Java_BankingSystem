40d89591468ebe587f324dee1cfa1c4fcd6ddcbd
Ritik Raj <hrr0077@gmail.com>
2021-06-02 18:18:11 +0530

    Secound Edit

--- Banking/Landing.java
+++ Banking/Landing.java
@@ -1,4 +1,16 @@
 class Landing{
+	long account_number;
+	String IFSC_code;
+	String Name;
+	String Address;
+	double Balance;
+
+	public void show(){
+		System.out.println("Account Number = "+ account_number);
+		System.out.println("Name ="+Name);
+		System.out.prrintln("Balance ="+Balance);
+	}
 	System.out.println("Welcome to the Java Bank");
 }
+
 
